8dd403b9eec0de63baafa74800c68c583ddf2410
ryanadolph7 <129120582+ryanadolph7@users.noreply.github.com>
2026-02-13 11:00:31 -0700

    Add files via upload

--- serverTest.py
+++ serverTest.py
@@ -1,9 +1,10 @@
 import time
 
 from flask import Flask, render_template, request, jsonify
-
+from maestro import Controller
 app = Flask(__name__)
 
+servo = Controller()
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -16,7 +17,12 @@ def joystick():
     y = data.get("y")
 
     print(f"Joystick X: {x}, Y: {y}")
-
+    yAxis = int(1200*float(y))
+    xAxis = int(1000 * float(x))
+    servo.setAccel(1, 5)
+    servo.setAccel(2,5)
+    servo.setTarget(1, 6000-yAxis-xAxis)
+    servo.setTarget(2, 6000+yAxis-xAxis)
     return jsonify({"status": "ok"})
 
 @app.route("/head", methods=["POST"])
@@ -24,8 +30,14 @@ def head_control():
     data = request.json
     tilt = data.get("tilt")
     pan = data.get("pan")
-
+    
     print(f"Head Tilt: {tilt}, Pan: {pan}")
+    panAmount = int(6000 + (1500 * float(pan)))
+    tiltAmount = int(6000 + (1500 * float(tilt)))
+    servo.setAccel(3, 4)
+    servo.setTarget(3, panAmount)
+    servo.setAccel(4, 4)
+    servo.setTarget(4, tiltAmount)
 
     return {"status": "ok"}
 
@@ -39,4 +51,4 @@ def buttons():
         print(f"Button {button} pressed")
     return {"status": "ok"}
 
-app.run(host="0.0.0.0", port=8888, debug=False)
+app.run(host="0.0.0.0", port=5000, debug=False)
