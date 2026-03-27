eea83789bd2a7cefcb8b07cfee0128b5124765b0
Nathan <nsrgy22@gmail.com>
2026-03-27 12:11:40 -0600

    if 'stopping' statement

--- lidar.py
+++ lidar.py
@@ -7,6 +7,11 @@ lidar = RPLidar(None, '/dev/ttyUSB0', timeout=3)
 try:
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
+            if(distance < 800):
+                lidar.stop()
+                lidar.disconnect()
+                print("Too close at angle", angle)
+                break
             print(f"Angle: {angle}, Distance: {distance}")
 except KeyboardInterrupt:
     print('Stopping.')
