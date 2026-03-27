1b37cb3e6eade816efcd23be60292e24402a009a
ryanadolph7 <129120582+ryanadolph7@users.noreply.github.com>
2026-03-27 12:48:30 -0600

    Add files via upload

--- lidar.py
+++ lidar.py
@@ -1,20 +1,18 @@
-import time
+import time 
 from adafruit_rplidar import RPLidar
 
 # Update port for your system (e.g., '/dev/ttyUSB0' for Linux)
 lidar = RPLidar(None, '/dev/ttyUSB0', timeout=3)
-
-try:
-    for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan:
-            if(distance < 800):
-                lidar.stop()
-                lidar.disconnect()
-                print("Too close at angle", angle)
-                break
-            print(f"Angle: {angle}, Distance: {distance}")
-except KeyboardInterrupt:
-    print('Stopping.')
-finally:
-    lidar.stop()
-    lidar.disconnect()
+counter = 0
+for scan in lidar.iter_scans():
+    counter += 1
+    print("New Scan " + str(counter))
+    for (_, angle, distance) in scan:
+        if((angle > 345) or (angle < 15)):
+            #front detection
+            if(distance < 400):
+                print("Too close at the front")
+        if((angle > 165) and (angle < 195)):
+            #back detection
+            if(distance < 400):
+                print("Too close at the back")
